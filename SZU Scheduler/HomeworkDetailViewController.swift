59b106a0472ecf145a7c604285a102f868b08039
Pencil <9807175@qq.com>
2017-07-27 00:27:47 +0800

    Hidden bottom bar on push and fixed scrollBar in homework detail page cannot be scrolled to end

---
+++ SZU Scheduler/HomeworkDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeworkDetailViewController.swift
+//  SZU Scheduler
+//
+//  Created by 陈林 on 27/07/2017.
+//  Copyright © 2017 Pencil. All rights reserved.
+//
+
+import UIKit
+
+class HomeworkDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
